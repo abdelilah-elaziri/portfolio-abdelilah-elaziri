cc99b5e8875940801f53399ba83fb6e4f62c5820
AbdelilahElaziri <abdelilahelaziri96@gmail.com>
2022-10-29 15:56:46 +0100

    Slidebar
    Design Slidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hi ! This is main portfolio</p>
-      <p>Abdelilah elaziri</p>
-    </div>
+    <>
+<Routes>
+  <Route path="/" element={<Layout/>}/>
+</Routes>
+    </>
   );
 }
 
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,48 @@
+import { Link, NavLink } from 'react-router-dom';
+import './index.scss';
+import LogoA from '../../assets/images/logo-s.png';
+import LogoSubtitle from '../../assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin, faSkype } from '@fortawesome/free-brands-svg-icons';
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to="/">
+            <img src={LogoA} alt='Logo' />
+            <img className="sub-logo" src={LogoSubtitle} alt='abdelilah' />
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="aboutme-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
+
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href="https://www.linkedin.com/in/abdelilah-elaziri-9b89a213a/">
+                    <FontAwesomeIcon icon={faLinkedin}  color="#4d4d4e"/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href="https://github.com/abdelilah-elaziri/">
+                    <FontAwesomeIcon icon={faGithub}  color="#4d4d4e"/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href="skype:live:abdelilahelaziri">
+                    <FontAwesomeIcon icon={faSkype}  color="#4d4d4e"/>
+                </a>
+            </li>
+        </ul>
+
+    </div>
+)
+
+export default Sidebar;
