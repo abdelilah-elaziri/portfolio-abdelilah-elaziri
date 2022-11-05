b38bc5e71d673f2a3aeb2f3f46932304014791cb
AbdelilahElaziri <abdelilahelaziri96@gmail.com>
2022-11-05 11:42:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/portfolio-abdelilah-elaziri" element={<Layout />} />
       </Routes>
     </>
   )
